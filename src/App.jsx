97f1902d2b557599bba1ea7145ef52ba8a98f638
Bernardo Maia <bblagesmaia@gmail.com>
2025-09-05 11:30:38 -0300

    trabalho feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='site'>
+      <img src="https://tm.ibxk.com.br/2022/11/22/22143022553305.jpg" alt="Rick" />
+
+      <h2>Rick</h2>
+      <p><strong>status:</strong>Vivinho</p>
+      <p><strong>Especie:</strong>Humano</p>
+      <p><strong>Genero:</strong>Masculino</p>
+      <p><strong>Ultimo ep:</strong> Ep. 41</p>
+    
+    </div>
   )
 }
 
 export default App
+;
