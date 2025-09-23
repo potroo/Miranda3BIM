2781e142062d2c662ae6061765cc6fa2e210926e
Bernardo Maia <bblagesmaia@gmail.com>
2025-09-23 17:14:50 -0300

    Prontinho

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,15 @@ function App() {
           <img src={personagem.image} alt={personagem.name} />
           <h2>{personagem.name}</h2>
           <p><strong>Status:</strong> {personagem.status}</p>
-          <p><strong>Espécie:</strong> {personagem.species}</p>
-          <p><strong>Gênero:</strong> {personagem.gender}</p>
+          <p><strong>species:</strong> {personagem.species}</p>
+          <p><strong>Gender:</strong> {personagem.gender}</p>
           <p><strong>Último ep:</strong> Ep. {personagem.episode.length}</p>
         </>
       )}
 
       <button onClick={() => {
         soma1()
-        console.log(count)
-      }}>
+        console.log(count)}}>
         Trocar de personagem
       </button>
     </div>
